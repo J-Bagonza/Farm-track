8e4106f00188219cc8b0a1ceff9d1a212e96f177
skidrow <136255106+Skidrow65@users.noreply.github.com>
2025-03-16 18:33:32 +0300

    add router

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,17 @@
+import { FaHandPointer } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import mainBackground from "/src/assets/images/mainbackground.jpg";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <section
       id="home"
       className="relative w-[98%] mx-auto h-[86vh] flex items-center bg-cover bg-center rounded-b-[3.8rem]"
       style={{ backgroundImage: `url(${mainBackground}) `}}
     >
-      {/* Overlay to improve text visibility */}
+      {/* Overlay for better text visibility */}
       <div className="absolute inset-0 bg-black bg-opacity-30 rounded-b-[3.8rem]"></div>
 
       {/* Text Content */}
@@ -25,8 +29,12 @@ const Home = () => {
           their resources
         </h3>
 
-        {/* Tracking Button */}
-        <button className="mt-6 bg-red-500 text-white font-bold py-3 px-6 rounded-lg hover:bg-green-700 transition duration-300">
+        {/* Tracking Button with Pointer Icon */}
+        <button
+          className="mt-6 flex items-center gap-2 bg-red-500 text-white font-bold py-3 px-6 rounded-lg hover:bg-green-700 transition duration-300"
+          onClick={() => navigate("/resource-tracker")}
+        >
+          <FaHandPointer className="text-2xl text-white animate-bounce" />
           Tap to track your Resources i.e. Animals...
         </button>
       </div>
