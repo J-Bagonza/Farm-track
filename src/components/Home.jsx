fcff7906e3f8d1307bd370bd3b0120fe7f6a93ce
skidrow <136255106+Skidrow65@users.noreply.github.com>
2025-03-17 21:08:49 +0300

    added responsivness to cart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,24 +8,24 @@ const Home = () => {
   return (
     <section
       id="home"
-      className="relative w-[98%] mx-auto h-[86vh] flex items-center bg-cover bg-center rounded-b-[3.8rem]"
-      style={{ backgroundImage:` url(${mainBackground}) `}}
+      className="relative w-[98%] mx-auto h-[78vh] md:h-[86vh] flex items-center bg-cover bg-center rounded-b-[2rem] md:rounded-b-[3.8rem]"
+      style={{ backgroundImage: `url(${mainBackground}) `}}
     >
       {/* Overlay for better text visibility */}
-      <div className="absolute inset-0 bg-black bg-opacity-40 rounded-b-[3.8rem]"></div>
+      <div className="absolute inset-0 bg-black bg-opacity-40 rounded-b-[2rem] md:rounded-b-[3.8rem]"></div>
 
       {/* Content Section */}
       <div className="relative z-10 px-6 md:px-16 lg:px-28 text-left">
-        <h2 className="text-white text-lg md:text-3xl font-bold">
+        <h2 className="text-white text-2xl md:text-3xl font-bold">
           Empowering Farmers
         </h2>
-        <h1 className="text-2xl md:text-5xl font-extrabold text-black">
+        <h1 className="text-3xl md:text-5xl font-extrabold text-black">
           All over <span className="text-red-500">Kenya</span>
         </h1>
-        <h3 className="text-lg md:text-2xl font-medium text-orange-300">
+        <h3 className="text-xl md:text-2xl font-medium text-orange-300">
           To get the best out of
         </h3>
-        <h3 className="text-xl md:text-3xl font-bold text-orange-600">
+        <h3 className="text-2xl md:text-3xl font-bold text-orange-600">
           their resources
         </h3>
 
